125316027d7caceba7b1f521e705a82c6882cc26
unknown <tranhadu17@gmail.com>
2022-12-05 12:44:27 +0700

    first commit

---
+++ chatter-api/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  AWS_ACCESS_KEY: 'AKIAQC4KQZS2IBNLFFFL',
+  AWS_SECRET_KEY: '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',
+  AWS_REGION: 'ap-southeast-1',
+  BUCKET: 'project-nhom-8', // s3 bucket name
+  ONESIGNAL_APP: '', // api id
+  ONESIGNAL_KEY: ''
+};
---
+++ chatter-app/src/config/constants.js
@@ -0,0 +1,5 @@
+export default {
+  base_url: 'http://192.168.1.5:8000',
+  bucket_url: 'https://project-nhom-8.s3.ap-southeast-1.amazonaws.com/',
+  onesignal_app: ''
+}
